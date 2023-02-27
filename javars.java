3d55ebcd2e3cfbdd1f0e47ab1a890844db8c18cf
nijimasu777 <118875574+nijimasu777@users.noreply.github.com>
2023-02-27 20:54:39 +0900

    Update javars.java

--- javars.java
+++ javars.java
@@ -6,18 +6,8 @@ public class javars {
         Random r = new Random();
         int bad = r.nextInt(3);
         System.out.println(bad);
-        if (kaitou < bad){
-            System.out.println("あなたの勝ちです" + hands[bad]);
-        }else if (kaitou > bad){
-            if (bad == 0){
-                System.out.println("あなたの勝ちです" + hands[bad]);
-                return;
-            }
+        if (kaitou < bad){System.out.println("あなたの勝ちです" + hands[bad]);}else if (kaitou > bad){if (bad == 0){System.out.println("あなたの勝ちです" + hands[bad]);return;}
             System.out.println("あなたの負けです" + hands[bad]);
-        }else if (kaitou == bad){
-            System.out.println("あいこです" + hands[bad]);
-        }
+        }else if (kaitou == bad){System.out.println("あいこです" + hands[bad]);}
     }
 }
-
-
